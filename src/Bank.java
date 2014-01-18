9f9e9b38c5f9a026c6d1d43dc52a439d41d42ed2
ArunaTebel <arunatebel@hotmail.com>
2014-01-18 17:47:08 +0530

    Merge origin/master

--- src/Bank.java
+++ src/Bank.java
@@ -1,9 +1,7 @@
 
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.Statement;
-import javax.swing.JOptionPane;
 
 public class Bank {
 
@@ -40,38 +38,13 @@ public class Bank {
         return bankCode;
     }
 
-    public void OpenDatabase() {
-
-        try {
-            Class.forName("com.mysql.jdbc.Driver");
-            conn = DriverManager.getConnection("jdbc:mysql://localhost/bogoda", "root", "");
-            System.out.println("Connected");
-
-        } catch (Exception e) {
-            System.out.println("Error in connection");
-        }
-    }
-
-    public void CloseDatabase() {
-        try {
-            conn.close();
-        } catch (SQLException e) {
-
-            e.printStackTrace();
-        }
-    }
-
     public void addToDataBase() {
-        OpenDatabase();
+        DatabaseManager dbCon = DatabaseManager.getDbCon();
         try {
-            Statement st = conn.createStatement();
-            // System.out.println(amount + accountType);
-            st.executeUpdate("INSERT INTO bank(Code,BankName) VALUES('" + bankCode + "','" + bankName + "')");
-            JOptionPane.showConfirmDialog(null, "Your Data Has been Inserted", "Result", JOptionPane.DEFAULT_OPTION, JOptionPane.PLAIN_MESSAGE);
-        } catch (Exception e) {
-            JOptionPane.showConfirmDialog(null, "Problem in Database connectivity  dsd or Data", "Result", JOptionPane.DEFAULT_OPTION, JOptionPane.WARNING_MESSAGE);
+            dbCon.insert("INSERT INTO bank(Code,BankName) VALUES('" + bankCode + "','" + bankName + "')");
+        } catch (SQLException ex) {
+            MessageBox.showMessage(ex.getMessage(), "SQL Error", bankCode);
         }
-        CloseDatabase();
 
     }
 
