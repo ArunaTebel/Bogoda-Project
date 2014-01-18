d872e51bd9234755c96de2eb137bdf186760dd06
Iddamalgoda <Iddamalgoda@Iddamalgoda_PC.connectify>
2014-01-18 13:51:18 -0800

    Merge origin/master

--- src/Bank.java
+++ src/Bank.java
@@ -1,5 +1,14 @@
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+import javax.swing.JOptionPane;
+
 public class Bank {
+
+    Connection conn = null;
+    Statement stmt = null;
 
     private String bankCode;
     private String bankName;
@@ -14,9 +23,9 @@ public class Bank {
         bankName = null;
     }
 
-    public void setBankName(String bankName, String bankCode) {
+    public void setBankName(String bankName) {
         this.bankName = bankName;
-        this.bankCode = bankCode;
+
     }
 
     public void setBankCode(String bankCode) {
@@ -31,7 +40,38 @@ public class Bank {
         return bankCode;
     }
 
+    public void OpenDatabase() {
+
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            conn = DriverManager.getConnection("jdbc:mysql://localhost/bogoda", "root", "");
+            System.out.println("Connected");
+
+        } catch (Exception e) {
+            System.out.println("Error in connection");
+        }
+    }
+
+    public void CloseDatabase() {
+        try {
+            conn.close();
+        } catch (SQLException e) {
+
+            e.printStackTrace();
+        }
+    }
+
     public void addToDataBase() {
+        OpenDatabase();
+        try {
+            Statement st = conn.createStatement();
+            // System.out.println(amount + accountType);
+            st.executeUpdate("INSERT INTO bank(Code,BankName) VALUES('" + bankCode + "','" + bankName + "')");
+            JOptionPane.showConfirmDialog(null, "Your Data Has been Inserted", "Result", JOptionPane.DEFAULT_OPTION, JOptionPane.PLAIN_MESSAGE);
+        } catch (Exception e) {
+            JOptionPane.showConfirmDialog(null, "Problem in Database connectivity  dsd or Data", "Result", JOptionPane.DEFAULT_OPTION, JOptionPane.WARNING_MESSAGE);
+        }
+        CloseDatabase();
 
     }
 
--- src/Reciepts_accounts.java
+++ src/Reciepts_accounts.java
@@ -14,14 +14,53 @@ public class Reciepts_accounts {
     String branchName; // this should be automatically filled..
     String chequeNo; // check whether we can use int here..
     String chequeDate;
-    Double amount;
-    String[] credit_accountCode;// there can be several credit entries
-    String[] credit_accountName;
-    String[] credit_description;
+    double debitAmount;
+    String[] credit_accountCode = new String[5];// there can be several credit entries
+    String[] credit_accountName = new String[5];
+    String[] credit_description = new String[5];
+    double[] creditAmount = new double[5];
     double difference; // allow save only if this is zero
+
+    public Reciepts_accounts() {
+        refNo = 0;
+        recieptNo = 0;
+        date = null;
+        payType = null;
+        debit_accountCode = null;
+        debit_accountName = null;
+        debit_description = null;
+        bankCode = null;
+        bankName = null;
+        branchCode = null;
+        branchName = null;
+        chequeDate = null;
+        debitAmount = 0;
+        for (int i = 0; i < 5; i++) {
+            credit_accountCode[i] = null;
+            credit_accountName[i] = null;
+            credit_description[i] = null;
+        }
+        for (int i = 0; i < 5; i++) {
+            creditAmount[i] = 0;
+        }
+
+    }
+
+    public void setRefNo(int refNo) {
+        this.refNo = refNo;
+    }
+    public void setRecieptNo(int recieptNo){
+        this.recieptNo=recieptNo;
+    }
+    public void setDate(String date){
+        this.date=date;
+    }
+    public void setPayType(String  payType){
+        this.payType=payType;
+    }
+    public void setDebit_accountCode(String debit_accountCode){
+        this.debit_accountCode=debit_accountCode;
+    }
     
-    
-    
-    
-    
+
 }
