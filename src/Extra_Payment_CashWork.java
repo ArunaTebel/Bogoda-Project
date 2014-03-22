863d9ea520558ef719680dc86b233e616ff1e811
Iddamalgoda <Iddamalgoda@Iddamalgoda_PC>
2014-03-22 19:28:23 +0530

    Merge origin/master

---
+++ src/Extra_Payment_CashWork.java
@@ -0,0 +1,29 @@
+public class Extra_Payment_CashWork {
+    
+    int emp_code;
+    String work_code;
+    int no_days;
+    double rate;
+    double amount;
+
+    // Constructers
+    
+    public Extra_Payment_CashWork() {
+        emp_code=0;
+        work_code=null;
+        no_days=0;
+        rate=0;
+        amount=0;
+    }
+    
+    // Setters
+    
+    public void Set_emp_code(int emp_code){
+        this.emp_code=emp_code;
+    }
+    
+    
+    
+    
+    
+}
